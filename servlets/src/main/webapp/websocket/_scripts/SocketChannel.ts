7ee41e30232e0c63c8222c7b5f535a49d66167cd
txtilya <txt.ilya@gmail.com>
2016-12-15 04:18:04 +0300

    New client

---
+++ servlets/src/main/webapp/websocket/_scripts/SocketChannel.ts
@@ -0,0 +1,59 @@
+//noinspection JSUnusedGlobalSymbols
+
+class SocketChannel {
+
+    private static readonly WS_URI = `ws://` + window.location.host + `/websocket/chat/1`;
+    private static readonly WSS_URI = `wss://` + window.location.host + `/websocket/chat/1`;
+
+    private input: HTMLInputElement;
+    private output: HTMLDivElement;
+    private webSocket: WebSocket;
+    private initParam: string;
+
+    //noinspection JSUnusedGlobalSymbols
+    constructor(outputElementId = `output`, inputElementId = `message`) {
+
+        this.output = document.getElementById(outputElementId) as HTMLDivElement;
+        this.input = document.getElementById(inputElementId) as HTMLInputElement;
+
+        this.input.form.addEventListener(`submit`, (evt: Event) => {
+            this.doSend(this.input.value);
+            this.input.value = "";
+            evt.preventDefault();
+        }, true);
+
+        this.initParam = window.location.protocol == `http:` ? SocketChannel.WS_URI : SocketChannel.WSS_URI;
+
+        // noinspection JSUnusedGlobalSymbols,SpellCheckingInspection
+        this.webSocket = Object.assign(new WebSocket(this.initParam), {
+            onerror: (evt: MessageEvent) => this.writeToScreen(`ERROR: ${evt.data}`),
+            onmessage: (evt: MessageEvent) => this.writeToScreen(`Message Received: ${evt.data}`),
+            onopen: () => this.writeToScreen(`Connected to Endpoint!`),
+        });
+
+        // noinspection SpellCheckingInspection
+        addEventListener(`beforeunload`,
+            this.webSocket.close
+                .bind(this.webSocket));
+    }
+
+
+    public writeToScreen(message: string) {
+        const paragraph = document.createElement(`p`);
+        paragraph.style.wordWrap = `break-word`;
+        paragraph.appendChild(document.createTextNode(message));
+        this.output.appendChild(paragraph);
+        while (this.output.childNodes.length > 25) {
+            this.output.removeChild(this.output.firstChild);
+        }
+        this.output.scrollTop = this.output.scrollHeight;
+
+    }
+
+    public doSend(message: string) {
+        if (message != ``) {
+            this.webSocket.send(message);
+            // this.writeToScreen(`Message Sent: ${message}`);
+        }
+    }
+}
